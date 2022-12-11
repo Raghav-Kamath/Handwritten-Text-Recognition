156836b188f834df65a8571045f656c45b07f30e
raghav <raghavkamath4@gmail.com>
2022-12-11 16:08:49 +0530

    first commit

---
+++ mylib/Config.py
@@ -0,0 +1,16 @@
+#===============================\Config./=======================================
+""" CONFIG v2: RANGE OF ARCHITECTURE FEATURES BELOW
+     -you can enable or disable them- """
+#===============================================================================
+# To test inference on webcam/video file. Set to True by default.
+Webcam = True
+# To correct text sentences.
+Correct = True
+# To save extracted text in a simple log.
+Log = False
+# Text translation from foreign lang. to English.
+Translate = False
+# Image processing to get better output.
+ImProc = True
+#===============================================================================
+#===============================================================================
